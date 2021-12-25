1d8a1df362e3f685bf3df32a51c87f81aadc6fc3
燕衡 <xueping.gxp@alibaba-inc.com>
2021-12-25 23:49:39 +0800

    Spring integrated with rabbitMQ

---
+++ spring-rabbitmq/src/main/java/com/chopin/springrabbitmq/tutorial/welcome/WelcomeReceiver.java
@@ -0,0 +1,8 @@
+package com.chopin.springrabbitmq.tutorial.welcome;/**
+ * @description: TODO 
+ * @author chopin
+ * @date 2021/12/25 23:30
+ * @version 1.0
+ */ 
+public class WelcomeReceiver {
+}
