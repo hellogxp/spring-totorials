a5ade611d2618c6d2a158ef839e3bf3abbf01a7d
燕衡 <xueping.gxp@alibaba-inc.com>
2021-12-25 23:52:07 +0800

    Spring integrated with rabbitMQ

--- spring-rabbitmq/src/main/java/com/chopin/springrabbitmq/tutorial/welcome/Config.java
+++ spring-rabbitmq/src/main/java/com/chopin/springrabbitmq/tutorial/welcome/Config.java
@@ -1,8 +1,34 @@
-package com.chopin.springrabbitmq.tutorial.welcome;/**
- * @description: TODO 
+package com.chopin.springrabbitmq.tutorial.welcome;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+/**
  * @author chopin
- * @date 2021/12/25 23:05
  * @version 1.0
- */ 
+ * @description: TODO
+ * @date 2021/12/25 23:05
+ */
+@Profile("welcome")
+@Configuration
 public class Config {
+
+    @Bean
+    public Queue welcome() {
+        return new Queue("welcome");
+    }
+
+    @Profile("receiver")
+    @Bean
+    public WelcomeReceiver receiver() {
+        return new WelcomeReceiver();
+    }
+
+    @Profile("sender")
+    @Bean
+    public WelcomeSender sender() {
+        return new WelcomeSender();
+    }
 }
--- spring-rabbitmq/src/main/java/com/chopin/springrabbitmq/tutorial/welcome/WelcomeReceiver.java
+++ spring-rabbitmq/src/main/java/com/chopin/springrabbitmq/tutorial/welcome/WelcomeReceiver.java
@@ -1,8 +1,18 @@
-package com.chopin.springrabbitmq.tutorial.welcome;/**
- * @description: TODO 
+package com.chopin.springrabbitmq.tutorial.welcome;
+
+import org.springframework.amqp.rabbit.annotation.RabbitHandler;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+
+/**
  * @author chopin
- * @date 2021/12/25 23:30
  * @version 1.0
- */ 
+ * @description: TODO
+ * @date 2021/12/25 23:30
+ */
+@RabbitListener(queues = "welcome")
 public class WelcomeReceiver {
+    @RabbitHandler
+    public void receive(String in) {
+        System.out.println(" [x] Received: '" + in + "'");
+    }
 }
